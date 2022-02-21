0ad84724adb7551c4a2e3aa6deca7c3a6f7f0833
deigo-lps <diego.lopes0909@gmail.com>
2022-02-21 18:57:15 -0300

    localStorage

--- src/store/items-context.js
+++ src/store/items-context.js
@@ -10,18 +10,30 @@ const ItemsContext = React.createContext({
 export const ItemsContestProvider = (props) => {
   const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    const newItems = JSON.parse(localStorage.getItem("data"));
+    if (newItems) {
+      setItems(newItems);
+    }
+  }, []);
+
   const addHandler = (item) => {
     setItems((items) => [item, ...items]);
   };
-  
+
   const deleteHandler = (key) => {
-    setItems((items) => items.filter((item) => item.key !== key));
+    setItems((items) => {
+      var newItems = items.filter((item) => item.key !== key);
+      localStorage.setItem("data", JSON.stringify(newItems));
+      return newItems;
+    });
   };
 
   const updateHandler = (item) => {
     setItems((items) => {
-      var newItems = [...items].filter((obj) => obj.key != item.key);
-      newItems.push(item);
+      var newItems = [...items].filter((obj) => obj.key !== item.key);
+      newItems.unshift(item);
+      localStorage.setItem("data", JSON.stringify(newItems));
       return newItems;
     });
   };
